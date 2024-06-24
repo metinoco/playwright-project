4e540bffec689ed2de8cc86ac87c5adad4e8440a
Monshy Tinoco <djmonshy_tinoco@hotmail.com>
2024-06-24 08:24:17 -0500

    Uso de getByRole

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,13 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import exp from "constants";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test ('Verificar que la página es accesible', async({page})=>{
+//   await page.goto('http://localhost:4100')
+// })
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("Get by Role", async ({ page }) => {
+  await page.goto("http://192.168.1.9:8080/Localizadores.html");
+  await expect(page.getByRole("heading", { name: "Sign up" })).toBeVisible();
+  await page.getByRole("checkbox", { name: "Subscribe" }).check();
+  await page.getByRole("button", {name: "Submit"}).click()
 });
