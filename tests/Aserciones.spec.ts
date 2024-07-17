028c10c95c699d8ced67eedb46b213dfe53e7bcf
Monshy Tinoco <djmonshy_tinoco@hotmail.com>
2024-07-17 09:26:27 -0500

    Uso de toBeEditable

--- tests/Aserciones.spec.ts
+++ tests/Aserciones.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import exp from "constants";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("http://192.168.1.9:8080/Aserciones.html");
+  await page.goto("http://192.168.1.8:8080/Aserciones.html");
 });
 
 test("Verificación de elementos attached", async ({ page }) => {
@@ -11,13 +11,21 @@ test("Verificación de elementos attached", async ({ page }) => {
 
 test("Verificación de estado de elementos", async ({ page }) => {
   const checkbox = page.locator("#checked-checkbox");
-//   await checkbox.uncheck();
+  //   await checkbox.uncheck();
   await expect(checkbox).toBeChecked();
   //Verificación negativa
-//   await expect(checkbox).not.toBeChecked();
+  //   await expect(checkbox).not.toBeChecked();
 });
 
 test("Verificación de elementos deshabilitados", async ({ page }) => {
-    const botonDeshabilitado = page.locator("#disabled-button");
-    await expect(botonDeshabilitado).toBeDisabled();
-  });
+  const botonDeshabilitado = page.locator("#disabled-button");
+  await expect(botonDeshabilitado).toBeDisabled();
+});
+
+test("Verificación de elementos editables", async ({ page }) => {
+  const textboxEditable = page.locator("#editable");
+  await expect(textboxEditable).toBeEditable();
+
+  // const textboxNoEditable = page.locator("#non_editable_textbox");
+  // await expect (textboxNoEditable).not.toBeEditable();
+});
